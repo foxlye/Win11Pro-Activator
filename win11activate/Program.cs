b8cf79527fb441fa7d8c530b97885231bb4c1c42
foxlye <104304007+foxlye@users.noreply.github.com>
2023-12-11 18:11:57 +0500

    Remove trash code from CheckAdminPrivileges and StepOne

--- win11activate/Program.cs
+++ win11activate/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
+using System.Security.Principal;
 
 namespace win11activate
 {
@@ -18,6 +19,18 @@ namespace win11activate
             }
            
         }
+
+        static void IsElevated()
+        {
+            bool isElevated;
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+
+            return isElevated;
+        }
         
         
         static void Main(string[] args)
@@ -26,19 +39,26 @@ namespace win11activate
 
             int current_state = get_state();
 
-            switch (current_state)
+            if (!IsElevated())
             {
-                case 0:
-                    StepOne();
-                    break;
-
-                case 1:
-                    StepSecond();
-                    break;
-
-                default: logger("Error state"); break;
+                logger("Restart with admin privileges");
             }
+            else
+            {
+                 switch (current_state)
+                {
+                    case 0:
+                        StepOne();
+                        break;
 
+                    case 1:
+                        StepSecond();
+                        break;
+
+                    default: logger("Error state"); break;
+                }
+            }
+            
             Console.ReadKey();
         }
 
@@ -50,38 +70,29 @@ namespace win11activate
             logger("FIRST ACTION");
             logger("Step 1");
             string text = cmdController("cscript /nologo C:\\Windows\\System32\\slmgr.vbs -rearm");
-            bool flag = !text.Contains("Access denied") && !text.Contains("0xC004D302");
-            if (flag)
-            {
-                logger("Step 2");
-                Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop").SetValue("PaintDesktopVersion", 4);
-                logger("Step 3");
-                cmdController("gpupdate");
-                logger("Step 4");
-                Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\svsvc\\KMS").SetValue("", "kms_4");
-                logger("Step 5");
-                Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\svsvc").SetValue("Start", 4);
-                logger("Step 6");
-                cmdController(string.Format("reg add {0}HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform{1} /v SkipRearm /t REG_DWORD /d 1 /reg:64 /f", '"', '"'));
-                logger("Step 7");
-                cmdController("gpupdate");
 
-                Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop").SetValue("Activator_state", 1);
-                logger("First action completed. Restart PC and run Activator.");
-            }
-            else
+            if (text.Contains("0xC004D302"))
             {
-                bool flag2 = text.Contains("0xC004D302");
-                if (flag2)
-                {
-                    logger("Failed activation shlindows Code: 0xC004D302");
-                }
-                bool flag3 = text.Contains("Access denied");
-                if (flag3)
-                {
-                    logger("Restart with admin privilegies");
-                }
+                logger("Failed activation shlindows Code: 0xC004D302");
+                return;
             }
+            
+            logger("Step 2");
+            Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop").SetValue("PaintDesktopVersion", 4);
+            logger("Step 3");
+            cmdController("gpupdate");
+            logger("Step 4");
+            Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\svsvc\\KMS").SetValue("", "kms_4");
+            logger("Step 5");
+            Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\svsvc").SetValue("Start", 4);
+            logger("Step 6");
+            cmdController(string.Format("reg add {0}HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform{1} /v SkipRearm /t REG_DWORD /d 1 /reg:64 /f", '"', '"'));
+            logger("Step 7");
+            cmdController("gpupdate");
+
+            Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop").SetValue("Activator_state", 1);
+            
+            logger("First action completed. Restart PC and run Activator.");
         }
 
         private static void StepSecond()
